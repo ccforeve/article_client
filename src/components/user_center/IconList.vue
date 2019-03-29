674566afc69c440544702fe4618d1464fb207181
unknown <itwutao@163.com>
2019-03-30 00:18:01 +0800

    个人中心调整

--- src/components/user_center/IconList.vue
+++ src/components/user_center/IconList.vue
@@ -1,30 +1,90 @@
 <template>
-  <div class="flexv func">
-    <router-link to="/user/basic" class="flexitem centerv card">
-      <i class="flex center bls bls-mp" style="background:#fbc45d; "></i>
+  <div class="flexv func divMargin">
+    <router-link
+      to="/user/basic"
+      class=" userLink"
+    >
+      <i
+        class="flex center bls bls-mp"
+        style="background:#fbc45d; "
+      ></i>
       <span class="flex center text">设置名片</span>
     </router-link>
-    <router-link to="/punch" class="flexitem centerv">
-      <i class="flex center bls bls-jhy" style="background:#67cef9; "></i>
+    <router-link
+      to="/punch"
+      class=" userLink"
+    >
+      <i
+        class="flex center bls bls-time"
+        style="background:#FF6633; "
+      ></i>
       <span class="flex center text">每日打卡</span>
     </router-link>
-    <router-link to="/invitation" class="flexitem centerv">
-      <i class="flex center bls bls-jhy" style="background:#67cef9; "></i>
-      <span class="flex center text">邀请好友<span>（可得30%佣金）</span></span>
+    <router-link
+      to="/invitation"
+      class=" userLink"
+      style=" position: relative;"
+    >
+      <i
+        class="flex center bls bls-jhy"
+        style="background:#67cef9; "
+      ></i>
+      <span class="flex center text">邀请好友</span>
+      <span class="spanR">30%佣金</span>
     </router-link>
-    <router-link to="/profit" class="flexitem centerv tg">
-      <i class="flex center bls bls-generalize" style="background:#35f921; "></i>
+    <router-link
+      to="/profit"
+      class=" userLink downMargin"
+    >
+      <i
+        class="flex center bls bls-generalize"
+        style="background:#35f921;"
+      ></i>
       <span class="flex center text">推广中心</span>
     </router-link>
-    <router-link to="/message" class="flexitem centerv tg">
-      <i class="flex center bls bls-consult" style="background:#ffc0cb"></i>
+    <router-link
+      to="/message"
+      class=" userLink"
+    >
+      <i
+        class="flex center bls bls-consult"
+        style="background:#ffc0cb"
+      ></i>
       <span class="flex center text">留言管理</span>
     </router-link>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "IconList"
-  }
+export default {
+  name: "IconList"
+};
 </script>
+<style>
+.divMargin {
+  margin-top: 0.5rem;
+  padding: 1.5rem 0 1rem;
+}
+.userLink {
+  width: 33.33%;
+  display: inline-block;
+}
+.downMargin{
+  margin:2rem 0 2.5rem;
+}
+.spanR{
+    font-size: 0.8rem;
+    position: absolute;
+    width: 4rem;
+    color: white;
+    border-radius: 0.3rem;
+    background: #FF0000;
+    text-align: center;
+    top: -0.6rem;
+    right: 1.2rem;
+}
+span.flex.center.text{
+  margin-top: 0.5rem;
+}
+</style>
+
--- vue.config.js
+++ vue.config.js
@@ -21,10 +21,11 @@ module.exports = {
   // 它支持webPack-dev-server的所有选项
   devServer: {
     host: "btl.yxcxin.com",
+    // host: "localhost",
     port: 80, // 端口号
     https: false, // https:{type:Boolean}
     open: true, //配置自动启动浏览器
-    // proxy: 'http://localhost:8080' // 配置跨域处理,只有一个代理
+    // proxy: 'http://localhost:8080' ,// 配置跨域处理,只有一个代理
 
     // 配置多个代理
     proxy: {
