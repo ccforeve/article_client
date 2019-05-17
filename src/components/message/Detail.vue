fd2e1c65fc6230539749d7f03ba9dcebda8fc23b
chunge <184364921@qq.com>
2019-05-17 19:58:53 +0800

    show message

--- src/components/message/Detail.vue
+++ src/components/message/Detail.vue
@@ -30,6 +30,7 @@
         <span class="">咨询内容：</span>
         <span>{{detail.message}}</span>
       </div>
+      <div class="delete-tip" v-if="!is_member && detail.show">如未开通会员联系客户，留言将于24小时后释放</div>
     </div>
     <!--end-->
     <div class="flexv centerv dredge" v-if="!is_member">
@@ -81,3 +82,9 @@
     }
   }
 </script>
+<style>
+  .delete-tip{
+    padding-top: 5px;
+    color: red;
+  }
+</style>
