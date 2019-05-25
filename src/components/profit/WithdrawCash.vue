d97d4906dbe242292f976ce7ebd12d72f901672c
chunge <184364921@qq.com>
2019-05-25 18:00:24 +0800

    edit withdraw_cash

--- src/components/profit/WithdrawCash.vue
+++ src/components/profit/WithdrawCash.vue
@@ -1,20 +1,20 @@
 <template>
-  <div id="cash" class="flexv wrap">
-    <div class="flexitemv mainbox">
+  <div class="flexv wrap">
+    <div id="cash" class="flexitemv mainbox" @click="closeService">
       <div class="cashes">
         <div class="flex center title">推广奖励申请提现</div>
-        <div class="flexv bank">
-          <template v-if="!user.ali_account">
-            <div class="flex"><span class="flexitem">未绑定账户</span></div>
-            <div class="flex"><em class="flexitem">您当前尚未绑定支付宝</em></div>
-            <router-link to="/profit/bind_account" class="flex endh centerv bls bls-yjt"></router-link>
-          </template>
-          <template v-else>
-            <div class="flex"><span class="flexitem">支付宝</span></div>
-            <div class="flex"><em class="flexitem">{{user.ali_name + ' ' + user.ali_account}} </em></div>
-            <router-link to="/profit/bind_account" class="flex endh centerv bls bls-yjt"></router-link>
-          </template>
-        </div>
+<!--        <div class="flexv bank">-->
+<!--          <template v-if="!user.ali_account">-->
+<!--            <div class="flex"><span class="flexitem">未绑定账户</span></div>-->
+<!--            <div class="flex"><em class="flexitem">您当前尚未绑定支付宝</em></div>-->
+<!--            <router-link to="/profit/bind_account" class="flex endh centerv bls bls-yjt"></router-link>-->
+<!--          </template>-->
+<!--          <template v-else>-->
+<!--            <div class="flex"><span class="flexitem">支付宝</span></div>-->
+<!--            <div class="flex"><em class="flexitem">{{user.ali_name + ' ' + user.ali_account}} </em></div>-->
+<!--            <router-link to="/profit/bind_account" class="flex endh centerv bls bls-yjt"></router-link>-->
+<!--          </template>-->
+<!--        </div>-->
         <h2 class="flex center">{{surplus_profit}}</h2>
         <p class="flex center">当前可提现余额</p>
         <a href="javascript:;" class="flex center getcash" :class="{'disable': !user.ali_account || !surplus_profit}"
@@ -22,9 +22,16 @@
       </div>
       <div class="flex center tips"><span>温馨提示</span></div>
       <div class="tip">
-        <p>1、单次提现最多200元</p>
+        <p>1、单次提现需大于100，最多200元</p>
         <p>2、提现金额最晚在下一个工作日24:00之前到账</p>
-        <p>3、周六、周日与节假日顺延至下一个工作日！</p>
+<!--        <p>3、周六、周日与节假日顺延至下一个工作日！</p>-->
+      </div>
+    </div>
+    <!--客服微信-->
+    <div class="twinkle flexv kf-qrcode" @click="service">
+      <div class="flex center kf-tit">提现客服</div>
+      <div class="flex center kf-img">
+        <img src="../../assets/image/service.jpg" class="fitimg">
       </div>
     </div>
   </div>
@@ -50,20 +57,24 @@
         return this.$store.state.surplus_profit;
       },
     },
-    mounted() {
-      this.init();
-    },
     activated() {
       if (this.$store.state.surplus_profit === null) {
         let _this = this;
         let profit = getProfit();
         profit.then(function (res) {
-          // _this.surplus_profit = res.surplus_profit
           _this.$store.commit("updateSurplusProfit", res.surplus_profit);
         });
       }
     },
     methods: {
+      service() {
+        $('.kf-qrcode').animate({width:'20rem',top:'20%',right:'17%',});
+        $(".kf-tit").animate({fontSize:'2.2em'});
+      },
+      closeService () {
+        $(".kf-qrcode").animate({top:'64%',right:'0',width:'6rem',});
+        $(".kf-tit").animate({fontSize:'1.2em'});
+      },
       getCash() {
         let _this = this;
         // 判断是否是会员，非会员不可提现
@@ -75,21 +86,21 @@
           return;
         }
         // 判断是否已绑定银行卡
-        if (!_this.user.ali_account) {
-          Toast({message: "请先绑定银行卡", duration: 1000});
-          setTimeout(function () {
-            _this.$router.push("/profit/bind_account");
-          }, 1000);
+        // if (!_this.user.ali_account) {
+        //   Toast({message: "请先绑定支付宝", duration: 1000});
+        //   setTimeout(function () {
+        //     _this.$router.push("/profit/bind_account");
+        //   }, 1000);
+        //   return;
+        // }
+        // 判断余额是否满足
+        if (!_this.surplus_profit) {
+          Toast({message: "余额不足", duration: 1500});
           return;
         }
         // 限制提现金额
         if (_this.surplus_profit < 100) {
-          Toast({message: "提现金额需满100", duration: 1000});
-          return;
-        }
-        // 判断余额是否满足
-        if (!_this.surplus_profit) {
-          Toast({message: "余额不足", duration: 1000});
+          Toast({message: "提现金额需满100", duration: 1500});
           return;
         }
         let trottle = null,
@@ -107,18 +118,15 @@
               trottle = true;
               //提现操作
               let withdraw_cash = withdrawCash({price: surplus_profit});
-              withdraw_cash
-                  .then(function (res) {
-                    Indicator.close();
-                    if (res.code === 201) {
-                      Toast({message: "提现成功", duration: 1000});
-                      _this.$store.commit("updateSurplusProfit", _this.surplus_profit - surplus_profit);
-                    }
-                  })
-                  .catch(function (e) {
-                    Indicator.close();
-                    Toast({message: e.msg, duration: 1000});
-                  });
+              withdraw_cash.then(function (res) {
+                  Indicator.close();
+                  Toast({message: "提现成功", duration: 1500});
+                  _this.$store.commit("updateSurplusProfit", _this.surplus_profit - surplus_profit);
+                }).catch(function (e) {
+                  Indicator.close();
+                  Toast({message: e.msg, duration: 1500});
+                  _this.$store.commit("updateSurplusProfit", _this.surplus_profit - surplus_profit);
+                });
               layer.close(index);
             }
           },
@@ -131,4 +139,5 @@
 
 <style scoped>
   @import "../../assets/css/layer.css";
+  #app{height: 100% !important;}
 </style>
